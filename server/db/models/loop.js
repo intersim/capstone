7b73d6686884f9362810e47571f955a87dd6ab5d
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-03-18 12:35:07 -0400

    add loop model

---
+++ server/db/models/loop.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    tags: [String],
+    publish: Boolean,
+    name: {
+        type: String,
+        unique: true
+    }
+    numUses: Number,
+    source: String,
+    category: {
+        type: String,
+        enum: ['rhythm', 'chord', 'melody']
+    }
+
+});
+
+module.exports = mongoose.model('Loop', schema);
