423259f64c86b9ca9df96bf36ca3357409e2562f
Mariya Bogorodova <mb2283@cornell.edu>
2016-03-19 15:05:44 -0400

    fixed dynamic reference

--- server/db/models/comments.js
+++ server/db/models/comments.js
@@ -1,6 +1,6 @@
 'use strict';
 var mongoose = require('mongoose');
-var context = require('mongoose-context-ref');
+
 
 var schema = new mongoose.Schema({
   author: {
@@ -12,36 +12,16 @@ var schema = new mongoose.Schema({
     type: String,
     required: true
   },
-  target: [mongoose.Schema.ObjectId]
-
+  target: {
+    kind: String,
+    item: {
+    type: mongoose.Schema.Types.ObjectId,
+    refPath: 'target.kind'
+    }
+  }
 })
 
-schema.plugin(context);
 
 mongoose.model('Comment', schema);
 
-/*
-the plugin adds fields:
-{
-  context_id: ObjectId,
-  context_type: String
-}
 
-to our model.
-
-when we QUUeerYYY for a comment we must do something like this though: 
-
-Comment.find({
-  context_type: 'User'/'Loop'/'Composition',
-  context_id: '532280fcfed4c6f00d0dce63'(its regular id)
-})
-
-ALTERNATIVELY, 
-
-Comment.withContext({
-  user/loop/composition: '532280fcfed4c6f00d0dce63'
-});
-
-withContext is a static mongoose-context-ref adds to our schema
-
-*/
