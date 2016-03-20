7cff69fdc7de2f406e7f63aba170382085c1e544
intersim <emily.intersimone@gmail.com>
2016-03-19 21:57:13 -0400

    progress on Fabric JS experiment

--- browser/js/experiment/fabric-experiment.js
+++ browser/js/experiment/fabric-experiment.js
@@ -8,38 +8,33 @@ for (var i = 0; i < (600 / grid); i++) {
   canvas.add(new fabric.Line([ 0, i * grid, 600, i * grid], { stroke: '#ccc', selectable: false }))
 }
 
-canvas.add(new fabric.Rect({ 
-  left: 100, 
-  top: 100, 
-  width: 50, 
-  height: 50, 
+var rect = new fabric.Rect({ 
+  left: 40, 
+  top: 40, 
+  width: 40, 
+  height: 40, 
   fill: '#faa', 
   originX: 'left', 
   originY: 'top',
-  centeredRotation: true
-}));
+  centeredRotation: true,
+  lockScalingY: true,
+  lockRotation: true,
+  lockScalingFlip: true,
+  rx: 10,
+  ry: 10
+});
 
-// function getMousePos(canvas, evt) {
-//     var rect = canvas.getBoundingClientRect();
-//     return {
-//       x: evt.clientX - rect.left,
-//       y: evt.clientY - rect.top
-//     };
-// }
-
-// add objects on click?
-// add new rectangles on grid
-canvas.on('mousedown', function() {
-  // console.log mouse location on mousedown?
-})
+//add double-click listener?
+// getCenterPoint()
+// hoverCursor - show note?
 
 // want them to only show middle-left controls and middle right-controls
-function observeBoolean(property) {
-  document.getElementById(property).onclick = function() {
-    canvas.item(0)[property] = this.checked;
-    canvas.renderAll();
-  };
-};
+// function observeBoolean(property) {
+//   document.getElementById(property).onclick = function() {
+//     canvas.item(0)[property] = this.checked;
+//     canvas.renderAll();
+//   };
+// };
 
 // the below function is throwing off the snap to grid function...
 // observeOptionsList('setControlVisible');
@@ -50,4 +45,39 @@ canvas.on('object:moving', function(options) {
     left: Math.round(options.target.left / grid) * grid,
     top: Math.round(options.target.top / grid) * grid
   });
+});
+
+// scale only in grid increments?
+canvas.on('object:scaling', function(options) { 
+  options.target.set({
+    left: Math.round(options.target.left / grid) * grid,
+    top: Math.round(options.target.top / grid) * grid,
+    // right: Math.round(options.target.right / grid) * grid,
+    // bottom: Math.round(options.target.bottom / grid) * grid
+  });
+});
+
+canvas.on('mouse:down', function(options){
+  if (options.target) return;
+
+  console.log('mouse down!');
+  canvas.add(new fabric.Rect({ 
+      left: options.e.clientX,
+      top: options.e.clientY,
+      width: 40, 
+      height: 40, 
+      fill: '#faa', 
+      originX: 'left', 
+      originY: 'top',
+      centeredRotation: true,
+      lockScalingY: true,
+      lockRotation: true,
+      lockScalingFlip: true,
+    })
+  );
+  console.log(options.e.clientX, options.e.clientY);
+});
+
+$('#delete').click(function () {
+  canvas.getActiveObject().remove();
 });
