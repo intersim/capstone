277d7c74f5a17f2f5fcc449d225d5fa38e75d623
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-03-21 12:40:58 -0400

    added delete and added param for trackId

--- server/app/routes/tracks.js
+++ server/app/routes/tracks.js
@@ -3,10 +3,6 @@ var models = require('../../db/models/');
 var Composition = models.Composition;
 var Track = models.Track;
 
-router.get('/', function(req, res, next) {
-
-});
-
 router.post('/', function(req, res, next) {
   var track = new Track(req.body);
   track.save()
@@ -20,18 +16,25 @@ router.post('/', function(req, res, next) {
     res.status(201).json(newTrack);
   })
   .then(null, next)
+});
+
+router.param('trackId', function(req, res, next) {
+  Track.findById(req.params.trackId)
+  .then(function(track){
+    if (!track || req.composition.tracks.indexOf(track._id) === -1) {
+      next(new Error("could not find track for this composition"));
+    } else {
+      req.track = track;
+      next();
+    }
+  })
 })
 
 router.put('/trackId', function(req, res, next) {
   var modifiedTrack;
 
-  Track.findById(req.params.trackId)
-  .then(function(track) {
-    if (track) {
-      track.set(req.body);
-      return track.save();
-    } else next(new Error("could not find this track"));
-  })
+  req.track.set(req.body);
+  req.track.save()
   .then(function(track) {
     modifiedTrack = track;
     req.composition.tracks.push(track._id);
@@ -43,6 +46,11 @@ router.put('/trackId', function(req, res, next) {
   .then(null, next);
 })
 
-
+router.delete('/trackId', function(req, res, next) {
+  req.track.remove({})
+  .then(function() {
+    res.status(204).send();
+  })
+})
 
 module.exports = router;
