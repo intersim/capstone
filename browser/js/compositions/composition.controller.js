1e919501e4bc1a296c93cdce2df10dc4e6c2c3da
Mariya Bogorodova <mb2283@cornell.edu>
2016-03-24 11:15:55 -0400

    signup and other fixes

--- browser/js/compositions/composition.controller.js
+++ browser/js/compositions/composition.controller.js
@@ -8,3 +8,9 @@ app.controller('CompositionEditor', function($scope, CompositionFactory){
 
   $scope.loopBucket = [ {_id:"56f16f274852b8ef37d15429", name: "loop1"}, {_id:"56f16f2e4852b8ef37d1542f", name: "loop2"} ]
 });
+
+app.controller('CompViewCtrl', function($scope){
+	$scope.composition = {creator: "Bonnie", title: "and Clyde", description: "dope tunes to cause havoc to", tags: ['nice'], tracks: [[], []]}
+	// $scope.composition = composition;
+
+})
