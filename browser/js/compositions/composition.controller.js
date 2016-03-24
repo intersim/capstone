d1112a76c92619692511820bededc8698db2b79b
Mariya Bogorodova <mb2283@cornell.edu>
2016-03-24 12:47:25 -0400

    small change

--- browser/js/compositions/composition.controller.js
+++ browser/js/compositions/composition.controller.js
@@ -9,7 +9,7 @@ app.controller('CompositionEditor', function($scope, CompositionFactory){
   $scope.loopBucket = [ {_id:"56f16f274852b8ef37d15429", name: "loop1"}, {_id:"56f16f2e4852b8ef37d1542f", name: "loop2"} ]
 });
 
-app.controller('CompViewCtrl', function($scope){
+app.controller('CompViewCtrl', function($scope, CompositionFactory){
 	$scope.composition = {creator: "Clyde", title: "Bonnie", description: "dope tunes to cause havoc to", tags: ['nice'], tracks: [[], []]}
   	$scope.comments = CompositionFactory.getCommentsById($scope.composition._id);
 	// $scope.composition = composition;
