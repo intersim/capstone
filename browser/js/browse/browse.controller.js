40dc851ee696eeb478794dd36b6978bbd4640d18
Mariya Bogorodova <mb2283@cornell.edu>
2016-03-27 18:56:41 -0400

    typo

--- browser/js/browse/browse.controller.js
+++ browser/js/browse/browse.controller.js
@@ -1,9 +1,9 @@
 'use strict';
 
-app.controller('BrowseCtrl', function($scope, $state, allCompositons, allLoops){
+app.controller('BrowseCtrl', function($scope, $state, allCompositions, allLoops){
 
 	$scope.composition = allCompositions;
 	$scope.loops = allLoops;
-	
+
 
 })
