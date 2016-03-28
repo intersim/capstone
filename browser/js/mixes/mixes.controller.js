86babdeed31f8e4b7f71e1294ac2b8545f447b47
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-03-28 16:14:03 -0400

    added functionality for selecting a mix to see its loops and details in a sidepanel

--- browser/js/mixes/mixes.controller.js
+++ browser/js/mixes/mixes.controller.js
@@ -2,6 +2,19 @@
 
 app.controller('MixesCtrl', function($scope, mixes){
 
+  $scope.selected = null;
+
   $scope.mixes = mixes;
+
+  $scope.select = function(mix) {
+    var loops = [];
+    mix.tracks.forEach(function(track) {
+      track.measures.forEach(function(measure) {
+        if (measure.loop) loops.push(measure.loop);
+      })
+    })
+    mix.loops = loops;
+    $scope.selected = mix;
+  }
 
 })
