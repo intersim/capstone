2265f38e15ff43363b196f3fe635b1993c20f7bc
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-03-31 14:16:08 -0400

    tracks from seed file are now unique

--- browser/js/mixes/mixes.controller.js
+++ browser/js/mixes/mixes.controller.js
@@ -1,20 +1,13 @@
 'use strict';
 
-app.controller('MixesCtrl', function($scope, mixes){
+app.controller('MixesCtrl', function($scope, mixes, MixFactory){
 
   $scope.selected = null;
 
   $scope.mixes = mixes;
 
   $scope.select = function(mix) {
-    console.log(mix);
-    var loops = [];
-    mix.tracks.forEach(function(track) {
-      track.measures.forEach(function(measure) {
-        if (measure.loop) loops.push(measure.loop);
-      })
-    })
-    mix.loops = loops;
+    mix.loops = MixFactory.getLoops(mix);
     $scope.selected = mix;
   }
 
